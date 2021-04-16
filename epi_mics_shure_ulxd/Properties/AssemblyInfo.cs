331081b415c2080b20927d165f7b9ead8ac5d736
Trevor Payne <tpayne@pepperdash.com>
2021-04-16 14:58:36 -0500

    Migrate to Main
    Fixes to nuget process

--- epi_mics_shure_ulxd/Properties/AssemblyInfo.cs
+++ epi_mics_shure_ulxd/Properties/AssemblyInfo.cs
@@ -4,4 +4,6 @@
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("epi_mics_shure_ulxd")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
